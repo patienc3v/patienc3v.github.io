958c852002afd53e20f8099311b4737592c2cdcc
patienc3v <forspam78@gmail.com>
2026-08-12 10:01:06 -0400

    NGS: Add missing file for the refactor

---
+++ ngs/script/divisions.js
@@ -0,0 +1,10 @@
+const DIVISION_ORDER = ['Storm', 'Heroic', 'Nexus', 'A', 'AE', 'AW', 'B', 'BE', 'BNE', 'BSE', 'BW', 'C', 'CE', 'CW', 'D', 'DE', 'DNE', 'DSE', 'DW', 'E', 'EE', 'EW'];
+
+const DIVISION_NAME = {
+    20: {
+        'D': 'DE'
+    },
+    22: {
+        'C': 'CE'
+    }
+}
